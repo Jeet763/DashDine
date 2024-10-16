728b3bf9aa4de87f6c7eeb7479c008ee51579531
Shubhajit Mukherjee <shubhajit.mukherjee@eviden.com>
2024-10-16 18:48:39 +0530

    Use of Hooks

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,12 +1,28 @@
 import RestaurantCard from "./RestaurantCard.js";
 import resList from "../utils/mockData.js";
+import { useState } from "react";
 
 const Body = () => {
+  const [listOfRestaurants, setListOfRestaurant] = useState(resList);
+  //1. [listOfRestaurants, setListOfRestaurant] is Array Destructuring
+  //2.SetListOfRestaurants is a call back function
   return (
     <div className="body">
-      <div className="search">Search</div>
+      <div className="filter"></div>
+      <button
+        className="filter-btn"
+        onClick={() => {
+          const filteredList = listOfRestaurants.filter(
+            (res) => res.data.avgRating > 4
+          );
+          setListOfRestaurant(filteredList);
+        }}
+      >
+        Top Rated Restaurants
+      </button>
+
       <div className="res-container">
-        {resList.map((restaurant) => {
+        {listOfRestaurants.map((restaurant) => {
           return (
             <RestaurantCard key={restaurant.data.id} resData={restaurant} />
           );
