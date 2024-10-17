ae33ae0e558f04e01999c8d16248adf077153ba1
Shubhajit Mukherjee <shubhajit.mukherjee@eviden.com>
2024-10-17 23:58:19 +0530

    Api call made

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,11 +1,24 @@
 import RestaurantCard from "./RestaurantCard.js";
 import resList from "../utils/mockData.js";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const Body = () => {
   const [listOfRestaurants, setListOfRestaurant] = useState(resList);
   //1. [listOfRestaurants, setListOfRestaurant] is Array Destructuring
   //2.SetListOfRestaurants is a call back function
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=21.99740&lng=79.00110&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+    const json = await data.json();
+    console.log(json);
+  };
+
   return (
     <div className="body">
       <div className="filter"></div>
