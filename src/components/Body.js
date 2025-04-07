5f9878fe205cda70b1f265e3b69ed636f53ee121
Shubhajit <shubhajitm1996@gmail.com>
2025-04-08 02:28:06 +0530

    setlist and setfiltered list

--- src/components/Body.js
+++ src/components/Body.js
@@ -15,6 +15,8 @@ const Body = () => {
     const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=21.1702401&lng=72.83106070000001&page_type=DESKTOP_WEB_LISTING");
     const json = await data.json();
     console.log(json);
+    setListOfRestaurants(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+        setFilteredRestaurants(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
   };
 
   if (listOfRestaurants.length === 0) {
