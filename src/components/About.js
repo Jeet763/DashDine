3b0737b9ecda972db8401ba8307a273c01a35e2a
Shubhajit <shubhajitm1996@gmail.com>
2025-04-08 23:12:45 +0530

    added routes + 2 pages

---
+++ src/components/About.js
@@ -0,0 +1,11 @@
+const About = () => {
+    return(
+        <div>
+            <h1>About</h1>
+            <h2>Shubhajit Mukherjee</h2>
+            <h2>ReactJS Developer</h2>
+        </div>
+    ) ;
+};
+
+export default About ; 
---
+++ src/components/ContactUs.js
@@ -0,0 +1,9 @@
+const ContactUs = () => {
+    return(
+        <div>
+            Mob No. 9108578753
+        </div>
+    )
+}
+
+export default ContactUs;
