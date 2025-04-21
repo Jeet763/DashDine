1176db777d9900d1718461bf82d3f829a7e14874
Shubhajit <shubhajitm1996@gmail.com>
2025-04-21 15:51:23 +0530

    added new custom hook -useRestaurantMenu

--- src/components/Error.js
+++ src/components/Error.js
@@ -6,9 +6,15 @@ const Error = () =>{
     return(
         <div>
             <h1>Ooooops!! Something is Wrong !! </h1>
-            <h3>{err.status}:{err.statusText}</h3>
+            {/* <h3>{err.status}:{err.statusText}</h3> */}
+            <h3>
+        {err.status && err.statusText
+          ? `${err.status}: ${err.statusText}`
+          : err.message || "Unknown Error"}
+      </h3>
+
         </div>
-    )
-}
+    );
+};
 
 export default Error ; 
--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -1,25 +1,28 @@
 import React from 'react';
-import { useState , useEffect } from 'react';
+// import { useState , useEffect } from 'react';
 import Shimmer from './Shimmer';
 import { useParams } from 'react-router-dom';
-import { MENU_API } from '../utils/constants';
+// import { MENU_API } from '../utils/constants';
+import useRestaurantMenu from '../utils/useRestaurantMenu';
 
 const RestaurantMenu = () => {
 
-    const[ resInfo , setResInfo ] = useState(null);
+    // const[ resInfo , setResInfo ] = useState(null);
     const {resId} = useParams();
 
-    useEffect(()=>{
-        fetchMenu();
-    },[]);
+    const resInfo = useRestaurantMenu(); 
 
-const fetchMenu= async() =>{
-    const data = await fetch(MENU_API + resId );
-    const json = await data.json();
-    console.log(json);  
-    // console.log(json?.data?.cards[2]?.card?.card?.info);
-    setResInfo(json.data);
-};
+//     useEffect(()=>{
+//         fetchMenu();
+//     },[]);
+
+// const fetchMenu= async() =>{
+//     const data = await fetch(MENU_API + resId );
+//     const json = await data.json();
+//     console.log(json);  
+//     // console.log(json?.data?.cards[2]?.card?.card?.info);
+//     setResInfo(json.data);
+// };
 
 if (resInfo === null )  return  <Shimmer />  ;
   
